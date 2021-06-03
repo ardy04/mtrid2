1af59ad960cc76b64de81a7d2a144e26b9546bc6
Denaya <48344579+DenayaMahabahYousi@users.noreply.github.com>
2021-06-03 20:49:15 +0700

    Add files via upload
    UI masih belum ada Dashboard dan belum dihubungkan antar activitynya

---
+++ AD/TemanIKM/app/src/main/java/com/mtrid2/temanikm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mtrid2.temanikm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
